57eab3925dbb4d30a69a97c421aaf5b585595486
Mark Ag <mk2modx@icloud.com>
2020-02-26 17:38:06 -0700

    added work

---
+++ automateBoring/ch5/characterCount.py
@@ -0,0 +1,9 @@
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)  #character is the key and the it is defaulted to 0
+    count[character] = count[character] + 1
+    # count gets incremented everytime the key is found
+
+print(count)
